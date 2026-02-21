62e69d84201488465d58adc59d0228e73bc908f9
viniciusP23 <158086630+viniciusP23@users.noreply.github.com>
2026-02-21 20:34:25 -0300

    projeto concluído

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
-  const imagens = document.querySelectorAll(".feedback-card img");
-  const modal = document.getElementById("modalImg");
-  const modalImg = document.getElementById("imgModalConteudo");
-  const fechar = document.querySelector(".fechar-modal");
+const imagens = document.querySelectorAll(".feedback-card img");
+const modal = document.getElementById("modalImg");
+const modalImg = document.getElementById("imgModalConteudo");
+const fechar = document.querySelector(".fechar-modal");
 
-  imagens.forEach(img => {
-    img.addEventListener("click", () => {
-      modal.style.display = "flex";
-      modalImg.src = img.src;
-    });
+imagens.forEach(img => {
+  img.addEventListener("click", () => {
+    modal.style.display = "flex";
+    modalImg.src = img.src;
   });
+});
 
-  fechar.addEventListener("click", () => {
+fechar.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
     modal.style.display = "none";
-  });
+  }
+});
 
-  modal.addEventListener("click", (e) => {
-    if (e.target === modal) {
-      modal.style.display = "none";
-    }
-  });
+///
+
+
+function enviarWhats(event) {
+
+  event.preventDefault()
+
+  const nome = document.getElementById("nome").value
+  const mensagem = document.getElementById("mensagem").value
+  const telefone = "5514997665295"
+
+  const texto = `Olá! me chamo ${nome}, ${mensagem}`
+  const msgFormatada = encodeURIComponent(texto)
+
+  const url = `https://whatsa.me/${telefone}/?t=${msgFormatada}`
+
+  window.open(url, "_blank")
+}
 
